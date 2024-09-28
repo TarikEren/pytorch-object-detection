9e6096ddf0d45ebd686cd0bd1ab0a66097f16a61
TarikEren <1210606015@nku.edu.tr>
2024-09-28 14:06:09 +0300

    Added device and temporary path values

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+# TODO: Make the user provide paths using args.
+
 from pathlib import Path
 
 import torch
 import torchvision
+import pandas as pd
 
 from utils import coco_to_dict
-
-# Set the device based on the available devices in the system.
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+from dataset import Dataset
 
 # Transforms for the training data.
 train_transform = torchvision.transforms.Compose(transforms=[
@@ -23,12 +24,25 @@ test_transform = torchvision.transforms.Compose(transforms=[
     torchvision.transforms.ToTensor(),
 ])
 
+# Set the device based on the available devices in the system.
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Model and its weights
 model_weights = torchvision.models.detection.FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
 model = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(weights=model_weights)
 
+# Temporary paths
+image_path: Path = Path("../datasets/cup_dataset/train")
+csv_path: Path = Path("out.csv")
+dataframe = pd.read_csv(csv_path)
+
 def main():
-    # TODO: Make the user provide the path using args.    
-    print(device, model)
+    
+    
+    dataset = Dataset(image_path=image_path,
+                      dataframe=dataframe,
+                      transforms=train_transform)
+           
     
 # Main function caller
 if __name__ == "__main__":
