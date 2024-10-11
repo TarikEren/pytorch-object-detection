023a2ab0f126ac3eee563e359eb932550d2660fd
TarikEren <1210606015@nku.edu.tr>
2024-10-11 12:46:23 +0300

    Changed up main entry point so that it contains less lines

--- main.py
+++ main.py
@@ -1,49 +1,62 @@
+# TODO: Write a testing loop
+# TODO: Write a training loop
+# TODO: Write a drawing function
 # TODO: Make the user provide paths using args.
 
-from pathlib import Path
+from ast import literal_eval
 
 import torch
-import torchvision
-import pandas as pd
+import torch.utils
+import torch.utils.data
+import torch.utils.data.dataloader
+from tqdm.auto import tqdm
 
-from utils import coco_to_dict
-from dataset import Dataset
+from dataset import Custom_Dataset
+from utils import collate_fn, get_coco_annotations
+from config import (output_text_path,
+                    train_path,
+                    coco,
+                    EPOCHS,
+                    BATCH_SIZE,
+                    NUM_WORKERS,
+                    model,
+                    device,
+                    train_transform)
 
-# Transforms for the training data.
-train_transform = torchvision.transforms.Compose(transforms=[
-    torchvision.transforms.Resize(size=(224, 224)),
-    torchvision.transforms.ToTensor(),
-    torchvision.transforms.RandomVerticalFlip(p=0.5),
-    torchvision.transforms.RandomHorizontalFlip(p=0.5),
-    torchvision.transforms.TrivialAugmentWide()
-])
 
-# Transforms for the testing data.
-test_transform = torchvision.transforms.Compose(transforms=[
-    torchvision.transforms.Resize(size=(224, 224)),
-    torchvision.transforms.ToTensor(),
-])
-
-# Set the device based on the available devices in the system.
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Model and its weights
-model_weights = torchvision.models.detection.FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn_v2(weights=model_weights)
-
-# Temporary paths
-image_path: Path = Path("../datasets/cup_dataset/train")
-csv_path: Path = Path("out.csv")
-dataframe = pd.read_csv(csv_path)
 
 def main():
+    annotation_list = get_coco_annotations(output_file=output_text_path, coco=coco)
+    train_dataset = Custom_Dataset(path_to_dataset=train_path, transforms=train_transform, annotation_list=annotation_list)
     
+    train_dataloader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                                batch_size=BATCH_SIZE,
+                                                num_workers=NUM_WORKERS,
+                                                collate_fn=collate_fn)
+
+    model.to(device=device)
+    # Set the model to training mode.
     
-    dataset = Dataset(image_path=image_path,
-                      dataframe=dataframe,
-                      transforms=train_transform)
-           
+    model.train()
+
     
+    for epoch in tqdm(range(EPOCHS), "Training model"):
+        
+        # Initialise the loss as 0.0
+        loss = 0.0
+        
+        for images, targets in train_dataloader:
+            # Send the images to the device
+            images = [image.to(device) for image in images]
+
+            # Send the targets to device
+            boxes = torch.tensor(targets["boxes"]).to(device)
+            classes = torch.tensor(targets["classes"]).to(device)
+            targets = {
+                "boxes": boxes,
+                "classes": classes
+            }
+
 # Main function caller
 if __name__ == "__main__":
     main()
