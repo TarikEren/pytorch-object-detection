bf268a129b9e9ac35a68c0db7f6abfc644b128a7
TarikEren <1210606015@nku.edu.tr>
2024-10-11 12:45:58 +0300

    Dataset class overhaul

--- dataset.py
+++ dataset.py
@@ -1,63 +1,52 @@
 from pathlib import Path
-from ast import literal_eval
 
-import pandas as pd
 import torch.utils.data
 import torch.utils.data.dataloader
 import torchvision
 from PIL import Image
 
-class Dataset(torch.utils.data.Dataset):
-    """
-    Custom dataset class to be used with a torch.utils.data.dataloader.
-    """
-    def __init__(self, 
-                 image_path: Path,
-                 dataframe: pd.DataFrame,
-                 transforms: torchvision.transforms):
-        
-        self.dataframe = dataframe
-        self.transforms = transforms
-        self.image_path = image_path
-
-    def __len__(self):
+class Custom_Dataset(torch.utils.data.Dataset):
+    def __init__(self,
+                 path_to_dataset: Path,
+                 transforms: torchvision.transforms,
+                 annotation_list: list) -> None:
         """
-        Returns the length of the dataset.
-        
-        Returns:
-            (int): Length of the dataset
-        """
-        return len(self.dataframe)
-        
-        
-    def __getitem__(self, idx):
-        """
-        Gets an image using the index and returns said image with its annotations.
+        Initialises the dataset
         
         Args:
-            idx (int): Index
+            path_to_dataset (Path): Path to the dataset
+            transforms (torchvision.transforms): Transforms to be applied to the image
+            annotation_list (list): List of images and their corresponding annotations
+        """
+       
+        # Initialise the variables
+        self.path_to_dataset: Path = path_to_dataset,
+        self.transforms: torchvision.transforms = transforms
+        self.annotation_list: list = annotation_list
+        self.images: list = list(path_to_dataset.glob("*.png"))
+    
+    def __len__(self):
+        """
+        Returns the amount of images
         
         Returns:
-            Tuple(torch.Tensor, torch.Tensor): A tuple of tensorized image with its annotations
+            (int): The image count
         """
-        image_name: str = self.dataframe["file_names"][idx]
+        return len(self.images)
         
-        try:
-            image: Image = Image.open(self.image_path / image_name)
-        except FileNotFoundError:
-            raise FileNotFoundError(f"Image {image_name} not found at path {self.image_path}")
+    def __getitem__(self, index):
+        # If no images found, return None
+        if self.__len__() == 0:
+            print("ERROR: No images found. Aborting")
+            return None
+        annotations = []
+        # Turn the extension uppercase as COCO stores extensions in uppercase
+        image_name = self.images[index].name.rstrip(".png") + ".PNG"
+        for entry in self.annotation_list:
+            if entry[0] == image_name:
+                image_path = self.path_to_dataset[0] / entry[0]
+                annotations.append(entry[1])
         
-        try:        
-            boxes = literal_eval(self.dataframe.iloc[idx]["box_coordinates"])
-            classes = literal_eval(self.dataframe.iloc[idx]["classes"])
-        except (ValueError, SyntaxError) as e:
-            raise ValueError(f"Error parsing annotations for index {idx}: {e}")
-        
-        labels = {
-            "boxes": torch.tensor(boxes, dtype=torch.float32),
-            "classes": torch.tensor(classes, dtype=torch.int64)
-        }
-        
-        image_tensor = self.transforms(image)
-        
-        return image_tensor, labels
+        image = Image.open(image_path)
+        image = self.transforms(image)
+        return image, annotations
